d0ac59a540c78cbc36be28a3d35b279fc8db2e51
Behackie <155098476+Behackie@users.noreply.github.com>
2026-08-01 19:43:03 +0000

    Tested ifstream for reading files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "../include/Airport.hpp"
 
 int main() {
-    std::cout << "Hello Airport Database!" << std::endl;
+    // 1. Open a pipe to the file
+    std::ifstream file("data/test.txt");
+
+    // 2. Safety check: make sure the file exists
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open data/test.txt!" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Successfully opened data/test.txt!\n--- File Contents ---" << std::endl;
+
+    // 3. Read line by line
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+
+    // 4. Close the file stream
+    file.close();
+
     return 0;
 }
